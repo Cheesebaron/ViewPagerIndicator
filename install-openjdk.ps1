920af2f882ab96a146431d4654337d5e78692db2
Tomasz Cielecki <TCielecki@emsbk.com>
2019-06-10 16:01:05 +0200

    Add script to install OpenJDK while AppVeyor images are derped

---
+++ install-openjdk.ps1
@@ -0,0 +1,27 @@
+$download_url = "https://dl.xamarin.com/OpenJDK/mac/microsoft-dist-openjdk-1.8.0.25.zip"
+
+Write-Host "Installing OpenJDK ..." -ForegroundColor Cyan
+
+$jdkPath = 'C:\Program Files\OpenJDK'
+
+if(Test-Path $jdkPath) {
+    Remove-Item $jdkPath -Recurse -Force
+}
+
+Write-Host "Downloading..."
+$zipPath = "$env:TEMP\microsoft-dist-openjdk-1.8.0.25.zip"
+(New-Object Net.WebClient).DownloadFile($download_url, $zipPath)
+
+Write-Host "Unpacking..."
+7z x $zipPath -oC:\openjdk_temp | Out-Null
+[IO.Directory]::Move('C:\openjdk_temp', $jdkPath)
+Remove-Item 'C:\openjdk_temp' -Recurse -Force
+del $zipPath
+
+Write-Host "Setting ENV var..."
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Progra~1\OpenJDK", "machine")
+[Environment]::SetEnvironmentVariable("JAVA_SDK", "C:\Progra~1\OpenJDK", "machine")
+$env:JAVA_HOME="C:\Progra~1\OpenJDK"
+$env:JAVA_SDK="C:\Progra~1\OpenJDK"
+
+Add-Path 'C:\Program Files\OpenJDK\bin'
